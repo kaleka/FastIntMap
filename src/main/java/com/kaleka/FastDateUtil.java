78254718239a9086d2c33f917db3fb7e474e2931
kaleka <kaleka@users.noreply.github.com>
2018-01-06 12:16:37 +0800

    FastSkipListMap

---
+++ src/main/java/com/kaleka/FastDateUtil.java
@@ -0,0 +1,158 @@
+package com.kaleka;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ *
+ * 
+ * @author kaleka
+ * 
+ */
+public final class FastDateUtil {
+
+	private static ReentrantLock lock = new ReentrantLock();
+
+	private static Calendar calendar = Calendar.getInstance();
+
+	public final static Date parseString2Date(final String input) {
+		try {
+			lock.lock();
+			String year = input.substring(0, 4);
+			String month = input.substring(5, 7);
+			String day = input.substring(8, 10);
+			String hour = input.substring(11, 13);
+			String minute = input.substring(14, 16);
+			String second = input.substring(17, 19);
+			calendar.set(Integer.parseInt(year), Integer.parseInt(month),
+					Integer.parseInt(day), Integer.parseInt(hour),
+					Integer.parseInt(minute), Integer.parseInt(second));
+			return calendar.getTime();
+		} catch (Exception e) {
+			return null;
+		} finally {
+			lock.unlock();
+		}
+	}
+
+	private final static String fillZero(final int input) {
+		if (input < 10)
+			return new StringBuilder().append('0').append(input).toString();
+		else
+			return Integer.toString(input);
+	}
+
+	public final static String getCurrentTime() {
+		try {
+			lock.lock();
+			calendar.setTime(new Date());
+			StringBuilder stringBuilder = new StringBuilder(8);
+			stringBuilder.append(fillZero(calendar.get(calendar.HOUR_OF_DAY)));
+			stringBuilder.append(':');
+			stringBuilder.append(fillZero(calendar.get(calendar.MINUTE)));
+			stringBuilder.append(':');
+			stringBuilder.append(fillZero(calendar.get(calendar.SECOND)));
+			return stringBuilder.toString();
+		} catch (Exception e) {
+			return null;
+		} finally {
+			lock.unlock();
+		}
+	}
+
+	public final static String formatHMS(Date date) throws Exception {
+		try {
+			lock.lock();
+			calendar.setTime(date);
+			StringBuilder stringBuilder = new StringBuilder(8);
+			stringBuilder.append(fillZero(calendar.get(calendar.HOUR_OF_DAY)));
+			stringBuilder.append(':');
+			stringBuilder.append(fillZero(calendar.get(calendar.MINUTE)));
+			stringBuilder.append(':');
+			stringBuilder.append(fillZero(calendar.get(calendar.SECOND)));
+			return stringBuilder.toString();
+		} catch (Exception e) {
+			throw e;
+		} finally {
+			lock.unlock();
+		}
+	}
+
+	public final static boolean isValidFormatHMS(String input){
+		try {
+			if(input.length()!=8)
+				return false;
+			String[] splitArray=input.split(":");
+			if(splitArray.length!=3)
+				return false;
+			for (int i = 0; i < splitArray.length; i++) {
+				if(splitArray[i].length()!=2)
+					return false;
+			}
+			int hour = Integer.parseInt(input.substring(0, 2));
+			if((hour>=24)||(hour<0))
+				return false;
+			int minute = Integer.parseInt(input.substring(3, 5));
+			if((minute>=60)||(minute<0))
+				return false;
+			int second = Integer.parseInt(input.substring(6, 8));
+			if((second>=60)||(second<0))
+				return false;
+			return true;
+		} catch (Exception e) {
+			return false;
+		} 
+	}
+
+	public final static String parseDateToString(final Date input) {
+		try {
+			lock.lock();
+			calendar.setTime(input);
+			StringBuilder stringBuilder = new StringBuilder(19);
+			stringBuilder.append(calendar.get(calendar.YEAR));
+			stringBuilder.append('-');
+			stringBuilder.append(fillZero(calendar.get(calendar.MONTH)+1));
+			stringBuilder.append('-');
+			stringBuilder.append(fillZero(calendar.get(calendar.DATE)));
+			stringBuilder.append(' ');
+			stringBuilder.append(fillZero(calendar.get(calendar.HOUR_OF_DAY)));
+			stringBuilder.append(':');
+			stringBuilder.append(fillZero(calendar.get(calendar.MINUTE)));
+			stringBuilder.append(':');
+			stringBuilder.append(fillZero(calendar.get(calendar.SECOND)));
+			return stringBuilder.toString();
+		} catch (Exception e) {
+			return null;
+		} finally {
+			lock.unlock();
+		}
+	}
+	
+	public final static String parseDateToStringYMD(final Date input) {
+		try {
+			lock.lock();
+			calendar.setTime(input);
+			StringBuilder stringBuilder = new StringBuilder(19);
+			stringBuilder.append(calendar.get(calendar.YEAR));
+			stringBuilder.append(fillZero(calendar.get(calendar.MONTH)+1));
+			stringBuilder.append(fillZero(calendar.get(calendar.DATE)));
+			return stringBuilder.toString();
+		} catch (Exception e) {
+			return null;
+		} finally {
+			lock.unlock();
+		}
+	}	
+
+	public static void main(String[] args) {
+		SimpleDateFormat sdf=new SimpleDateFormat("HH:mm:ss");
+		long start = System.currentTimeMillis();
+		for (int i = 0; i < 1000000; i++)
+			FastDateUtil.isValidFormatHMS("01:01:01");
+		System.out.println("cost time:" + (System.currentTimeMillis() - start)
+				+ "ms");
+	}
+
+}
---
+++ src/main/java/com/kaleka/FastSkipListMap.java
@@ -0,0 +1,86 @@
+package com.kaleka;
+
+import java.util.Date;
+import java.util.concurrent.ConcurrentSkipListMap;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class FastSkipListMap<T> {
+
+	private static Lock lock = new ReentrantLock();
+
+	private T data[] = null;
+
+	public FastSkipListMap(int size) {
+		data = (T[]) new Object[size];
+	}
+
+	public T get(int key) {
+		if (key > data.length - 1)
+			return null;
+		else
+			return data[key];
+	}
+
+	public void put(int key, T value) {
+		lock.lock();
+		try {
+			// update object state
+			int dataLength = data.length;
+			if (key > dataLength - 1) {
+				T dataNew[] = null;
+				System.arraycopy(data, 0,
+						(dataNew = (T[]) new Object[key<<1]), 0,
+						dataLength);
+				data = dataNew;
+			}
+			data[key] = value;
+		} finally {
+			lock.unlock();
+		}
+	}
+
+	public boolean containsKey(int key) {
+		if (key > data.length - 1)
+			return false;
+		else
+			return data[key] != null;
+	}
+
+	public static void testFastSkipListMap() {
+		int key=Integer.parseInt(FastDateUtil.parseDateToStringYMD(new Date()),10);
+		FastSkipListMap<Double> d=new FastSkipListMap<Double>(16);
+		for (int i = 0; i < 1000000; i++) {
+              d.put(key, 1d);			
+		}
+		long start = System.currentTimeMillis();
+		for (int i = 0; i < 2000000000; i++) {
+           d.get(key);			
+		}
+		System.out.println(System.currentTimeMillis()-start);
+	}
+
+	public static void testSkipListMap() {
+		long start = System.currentTimeMillis();
+		ConcurrentSkipListMap<Long,Double> d=new ConcurrentSkipListMap<Long,Double>();
+		long key=System.currentTimeMillis();
+		for (int i = 0; i < 1000000; i++) {
+              d.put(key, 1d);			
+		}
+		for (int i = 0; i < 2000000000; i++) {
+            d.get(key);			
+		}
+		System.out.println(System.currentTimeMillis()-start);
+	}
+	
+    public static void main(String[] args) {
+		testSkipListMap();
+		//testFastSkipListMap();
+		 System.out.println(Runtime.getRuntime().totalMemory());
+		 System.out.println(Runtime.getRuntime().freeMemory());
+		
+	}
+	
+	
+	
+}
